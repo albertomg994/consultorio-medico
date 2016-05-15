9bac4fb69b7966a462b3403f9fa7c008c7fc8909
Alberto Miedes Garcés <albertomg994@gmail.com>
2016-05-15 13:21:19 +0200

    Appointment-related TADs

---
+++ Appointment.h
@@ -0,0 +1,46 @@
+//
+//  Appointment.h
+//  T10-Juez-I
+//
+//  Created by Alberto Miedes on 13/5/16.
+//  Copyright © 2016 Alberto Miedes. All rights reserved.
+//
+
+#ifndef Appointment_h
+#define Appointment_h
+
+#include <string>
+
+#include "Date.h"
+#include "Clinic.h"
+
+using namespace std;
+
+using Medic = std::string;     // Medic type
+using Patient = std::string;   // Patient type
+
+/**
+ * Appointment class. An appointment is a patient-date association.
+ */
+class Appointment {
+private:
+    
+    Patient _patient;
+    Date _date;
+    
+public:
+    
+    // Constructor - create new appointment
+    Appointment(Patient p, Date d) : _patient(p), _date(d) {}
+    
+    // Getters
+    const Patient & patient() const { return _patient; }
+    const Date & date() const { return _date; }
+    
+    // Others
+    string to_string() {
+        return "Patient: " + _patient + " - Date: " + _date.to_string();
+    }
+};
+
+#endif /* Appointment_h */
