7309b273b8e6426dcb4cfa15ef9327c2a3ded29e
Alberto Miedes Garcés <albertomg994@gmail.com>
2016-05-19 13:25:20 +0200

    Main class

---
+++ main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  T10-Juez-I
+//
+//  Created by Alberto Miedes on 13/5/16.
+//  Copyright © 2016 Alberto Miedes. All rights reserved.
+//
+
+#include <iostream>
+#include "Clinic.h"
+#include <cassert>
+
+using namespace std;
+
+using Medic = std::string;     // Medic type
+using Patient = std::string;   // Patient type
+
+int main(int argc, const char * argv[]) {
+
+    // ...
+    
+    return 0;
+}
