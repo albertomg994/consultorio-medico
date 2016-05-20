55038c2a633f01fddfecedcdb0b6f8e0d6537478
Alberto Miedes Garcés <albertomg994@gmail.com>
2016-05-20 11:20:33 +0200

    Input processing for automated judge

--- ClinicIO.h
+++ ClinicIO.h
@@ -2,15 +2,146 @@
 //  ClinicIO.h
 //  T10-Juez-I
 //
-//  Created by Alberto Miedes on 14/5/16.
+//  Created by Alberto Miedes on 20/5/16.
 //  Copyright © 2016 Alberto Miedes. All rights reserved.
 //
 
 #ifndef ClinicIO_h
 #define ClinicIO_h
 
-// NOTA: Los métodos del TAD Consultorio no deben mostrar nada por pantalla.
-// El manejo de la entrada y salida de datos se realizará en funciones externas
-// al TAD.
+#include <string>
+#include <iostream>
+#include "Clinic.h"
+
+using namespace std;
+
+using Medic = string;     // Medic type
+using Patient = string;   // Patient type
+
+class ClinicIO {
+public:
+    
+    // No genera salida
+    static void nuevoMedico(Clinic &c) {
+        
+        string medico;
+        
+        cin >> medico;
+        
+        //cout << "nuevoMedico(" << medico << ")" << endl;
+        
+        c.nuevoMedico(medico);
+    }
+    
+    // No genera salida
+    static void pideConsulta(Clinic &c) {
+        
+        string paciente, medico;
+        unsigned int dia, hora, minuto;
+        
+        cin >> paciente;
+        cin >> medico;
+        cin >> dia >> hora >> minuto;
+        
+        
+        
+        //cout << "pideConsulta(" << paciente << ", " << medico << ", " << "fecha" << ")" << endl;
+        
+        //cout << "d: " << dia << " h: " << hora << " m: " << minuto << endl;
+        Date d(dia, hora, minuto);
+        
+        try {
+            c.pideConsulta(paciente, medico, d);
+        } catch (MedicNotFoundException mnfe) {
+            cout << mnfe.msg() << endl;
+            cout << "---" << endl;
+        } catch (DateAlreadyTakenException date) {
+            cout << date.msg() << endl;
+            cout << "---" << endl;
+        }
+    }
+    
+    // No genera salida
+    static void atiendeConsulta(Clinic &c) {
+        
+        string medico;
+        
+        cin >> medico;
+        
+        //cout << "atiendeConsulta(" << medico << ")" << endl;
+        
+        try {
+            c.atiendeConsulta(medico);
+        } catch (MedicNotFoundException mnfe) {
+            cout << mnfe.msg() << endl;
+            cout << "---" << endl;
+        } catch (EmptyAppointmentListException eale) {
+            cout << eale.msg() << endl;
+            cout << "---" << endl;
+        }
+    }
+    
+    // SI genera salida
+    static void listaPacientes(Clinic &c) {
+        
+        string medico;
+        unsigned int dia;
+        
+        cin >> medico;
+        cin >> dia;
+        
+        //cout << "d: " << dia << " h: " << 0 << " m: " << 0 << endl;
+        Date d_lista(dia, 0, 0);
+        
+        //cout << "listaPacientes(" << medico << ", " << "fecha" << ")" << endl;
+        
+        try {
+            list<struct PatientDate> l = c.listaPacientes(medico, d_lista);
+            cout << "Doctor " << medico << " dia " << d_lista.day() << endl;
+            list<struct PatientDate>::const_iterator it = l.cbegin();
+            if (l.size() == 0) cout << endl;
+            while (it != l.cend()) {
+                cout << format_patient_date(*it) << endl;
+                it++;
+            }
+            cout << "---" << endl;
+        } catch (MedicNotFoundException mnfe) {
+            cout << mnfe.msg() << endl;
+            cout << "---" << endl;
+        }
+    }
+    
+    // SI genera salida
+    static void siguientePaciente(Clinic &c) {
+        
+        string medico;
+        
+        cin >> medico;
+        
+        //cout << "siguientePaciente(" << medico << ")" << endl;
+        
+        cout << "Siguiente Paciente doctor " << medico << endl;
+        
+        try {
+            string sig_pac = c.siguientePaciente(medico);
+            //cout << "Siguiente Paciente doctor " << medico << endl;
+            cout << sig_pac << endl;
+            cout << "---" << endl;
+        } catch (MedicNotFoundException mnfe) {
+            cout << mnfe.msg() << endl;
+            cout << "---" << endl;
+        } catch (EmptyAppointmentListException eale) {
+            cout << eale.msg() << endl;
+            cout << "---" << endl;
+        }
+        
+    }
+    
+    // No genera salida
+    static void operacionNoValida() {
+        //cout << "Operación no válida" << endl;
+    }
+
+};
 
 #endif /* ClinicIO_h */
--- main.cpp
+++ main.cpp
@@ -7,17 +7,70 @@
 //
 
 #include <iostream>
+#include <string>
+#include <list>
 #include "Clinic.h"
-#include <cassert>
+#include "Date.h"
+#include "ClinicIO.h"
 
 using namespace std;
 
-using Medic = std::string;     // Medic type
-using Patient = std::string;   // Patient type
+using Medic = string;     // Medic type
+using Patient = string;   // Patient type
 
 int main(int argc, const char * argv[]) {
 
-    // ...
-    
+    unsigned int num_ops;
+    string operacion;
+
+    Clinic c;
+
+    // Read number of operations in first case
+    cin >> num_ops;
+    cin.ignore();
+
+    // Para cada caso
+    while (!cin.eof()) {
+
+        for (int i = 0; i < num_ops; i++) {
+
+            cin >> operacion;
+
+            if (operacion == "nuevoMedico") {
+                ClinicIO::nuevoMedico(c);
+            }
+
+            else if (operacion == "pideConsulta") {
+                ClinicIO::pideConsulta(c);
+            }
+
+            else if (operacion == "atiendeConsulta") {
+                ClinicIO::atiendeConsulta(c);
+            }
+
+            else if (operacion == "listaPacientes") {
+                ClinicIO::listaPacientes(c);
+            }
+
+            else if (operacion == "siguientePaciente") {
+                ClinicIO::siguientePaciente(c);
+            }
+
+            else {
+                ClinicIO::operacionNoValida();
+            }
+
+            cin.ignore();
+
+        }
+
+        // Each case ends with six -
+        cout << "------" << endl;
+
+        // Read number of operations in next case (if exists)
+        cin >> num_ops;
+        cin.ignore();
+    }
+
     return 0;
 }
